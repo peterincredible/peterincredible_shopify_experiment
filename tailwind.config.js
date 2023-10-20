b80325e3d6a4a602c56200615adced4c4deeeb46
peter dibie <ekene.dibie@gmail.com>
2023-10-20 06:19:09 +0100

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./sections/*.liquid",
+    "./templates/*.liquid",
+    "./templates/customers/*.liquid",
+    "./layout/*.liquid",
+    "./snippets/*.liquid",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
